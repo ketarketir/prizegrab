619518494951c0e3acd969493ecf45a97042a56e
anaksulung99 <60265725+anaksulung99@users.noreply.github.com>
2025-09-15 11:17:35 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -7,14 +7,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			images: {
-				sizes: [640, 828, 1200, 1920, 3840],
-				formats: ['image/avif', 'image/webp', 'image/png', 'image/jpg'],
-				minimumCacheTTL: 300,
-				domains: ['prizegrab.vercel.app'],
-			}
-		}),
+		adapter: adapter: adapter(),
 		alias: { '@/*': './src/lib/*' }
 	}
 };
